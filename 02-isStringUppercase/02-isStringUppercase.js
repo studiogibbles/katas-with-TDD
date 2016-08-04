a3ed4b096bb6ec96e36497f73f89911df777d2aa
gilbertjacke <gilbertjacke@gmail.com>
2016-08-04 10:07:41 +0100

    isStringUpperCase & Counting Monkeys done

---
+++ 02-isStringUppercase/02-isStringUppercase.js
@@ -0,0 +1,22 @@
+<<<<<<< Updated upstream
+=======
+var stringUpper = function(string){
+  var result = false;
+
+  var regex = /[^A-Za-z]/;
+
+  if(string.match(regex)){
+    // TODO: use regex.test(string) - returns boolean
+  return 'Invalid input';
+  }
+  if(string === string.toUpperCase()){
+    return true;
+    }else {
+    return false;
+  }
+
+return result;
+};
+
+module.exports=stringUpper;
+>>>>>>> Stashed changes
---
+++ spec/02-isStringUppercase.js
@@ -0,0 +1,32 @@
+<<<<<<< Updated upstream
+=======
+import { expect } from 'chai';
+import stringUpper from '../02-isStringUpperCase/02-isStringUpperCase';
+
+describe.only('stringUpper()', function () {
+  it('should be a function', function () {
+    expect(stringUpper).to.be.a('function');
+  });
+  it('takes one argument', function () {
+    expect(stringUpper.length).to.equal(1);
+  });
+  it('returns a boolean', function () {
+    expect(stringUpper('string')).to.equal(false);
+  });
+  it('return a true for HELLO', function () {
+    expect(stringUpper('HELLO')).to.equal(true);
+  });
+  it('return a false for Hello', function () {
+    expect(stringUpper('Hello')).to.equal(false);
+  });
+  it('return a false for HeLLo', function () {
+    expect(stringUpper('HeLLo')).to.equal(false);
+  });
+  it('return a false for HeLLo', function () {
+    expect(stringUpper('HeLLo')).to.equal(false);
+  });
+  // it('return a false for HeLLo', function () {
+  //   expect(stringUpper('He£££Lo')).to.equal('Invalid input');
+  // });
+});
+>>>>>>> Stashed changes
