ce53f7f5e1a074caff377f620a93337e83499151
gilbertjacke <gilbertjacke@gmail.com>
2016-08-31 19:15:13 +0100

    prizeDraw - 2 tests passing

---
+++ 07-prizeDraw/07-prizeDraw.js
@@ -0,0 +1,54 @@
+var prizeDraw = function (array){
+  //create a list of objects with the ranks
+  //loop through array and ranks
+  //split the array into indvidual letters
+  //
+
+  var ranks = {
+    a:1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 18,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26
+  };
+
+
+  var result = "";
+
+  var split = array.split().toLowerCase();
+  console.log(split);
+
+  split.forEach(function (rank) {
+
+  });
+
+
+
+
+  return result
+
+
+};
+
+module.exports = prizeDraw;
