447a50c174ba402b368b0be1d51d3f05d2913338
gilbertjacke <gilbertjacke@gmail.com>
2016-10-03 09:02:59 +0100

    3 tests passing

---
+++ 08-twoToOne/08-twoToOne.js
@@ -0,0 +1,26 @@
+var twoToOne = function (str1, str2) {
+
+  var arr1 = str1.split("");
+  var arr2 = str2.split("");
+
+
+  var newArr = arr1.concat(arr2);
+
+
+  function unique(newArr) {
+    return newArr.sort().filter(function (item, pos, arr) {
+      return !pos || item !== arr[pos - 1];
+    })
+  }
+
+  var join = unique(newArr).join('');
+
+  return   join;
+
+
+};
+
+console.log(twoToOne('katie', 'bob'));
+console.log(twoToOne('aacc', 'bcbb'));
+
+module.exports = twoToOne;
