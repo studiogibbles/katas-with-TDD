a65b4964fd52010f09cb86ecc0255df6d14e563e
gilbertjacke <gilbertjacke@gmail.com>
2016-10-11 11:26:36 +0100

    6 tests passing

---
+++ 11-whatTimeIsIt/11-whatTimeIsIt.js
@@ -0,0 +1,27 @@
+var whatTimeIsIt = function (str) {
+
+  var arr = str.split('');
+  if (arr[8] === 'A' && arr[1] === '2') {
+    return '00:00:00';
+  }
+  if (arr[8] === 'P' &&  arr[1] === '2') {
+    return '24:00:00';
+  }
+  if (arr[8] === 'A') {
+    return str.slice(0, -2);
+  }
+  if (arr[8] === 'P') {
+      var slice = str.slice(0, -2);
+      var hrs = +slice.substring(0,2) + 12;
+      var newStr = slice.slice(2);
+    return hrs + newStr;
+  }
+
+};
+
+console.log(whatTimeIsIt('12:00:00AM'));
+console.log(whatTimeIsIt('12:00:00PM'));
+console.log(whatTimeIsIt('09:25:11AM'));
+console.log(whatTimeIsIt('06:05:00PM'));
+
+module.exports = whatTimeIsIt;
