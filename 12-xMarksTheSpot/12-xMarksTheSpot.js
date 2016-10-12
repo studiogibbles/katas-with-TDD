0ec2716ad67fa59de1d298f8935838ec18d2c34a
gilbertjacke <gilbertjacke@gmail.com>
2016-10-12 15:35:05 +0100

    4 tests passing

---
+++ 12-xMarksTheSpot/12-xMarksTheSpot.js
@@ -0,0 +1,31 @@
+var xMarks = function (arr) {
+//set a new array
+//find the array where there is an x
+// find the index where there is an x
+
+
+  var result = [];
+
+  arr.forEach(function (el, i) {
+    var array = el;
+    array.forEach(function (el, j) {
+      if (el === 'x') {
+        result.push(i);
+        result.push(j);
+      }
+    });
+  });
+
+  return result;
+
+
+};
+
+console.log(xMarks([
+  ['o', 'o', 'o'],
+  ['o', 'o', 'o'],
+  ['x', 'o', 'o']
+]));
+
+
+module.exports = xMarks;
